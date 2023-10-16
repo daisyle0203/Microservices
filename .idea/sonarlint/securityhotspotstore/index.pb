66c4105fde84e5ebd112cbfff81b5d2767579c89
Daisy <daisy.le0203@gmail.com>
2023-10-15 20:18:26 -0500

    setting up dynamic port in the response

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-Q
-!currency-exchange-service/pom.xml,a/d/ad82e8ab52b8b61bd715da8abbd34b7bec0cdf55
--- currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.daisy.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,19 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    // Get the value of the port
+    @Autowired
+    private Environment environment;
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to
     ) {
-        return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(50));
+        // Extract the port from the environment and set it into the response
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
