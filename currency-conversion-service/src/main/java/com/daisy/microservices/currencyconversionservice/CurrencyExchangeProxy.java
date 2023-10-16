925f82cadcbf4a2752df9c20ddb4fc5a5f7530b0
Daisy <daisy.le0203@gmail.com>
2023-10-16 16:36:28 -0500

    add the eureka client

--- currency-conversion-service/src/main/java/com/daisy/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/daisy/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -3,7 +3,7 @@ package com.daisy.microservices.currencyconversionservice;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+// this is the interface that will be used to call the currency exchange service using feign
 @FeignClient(name = "currency-exchange", url="localhost:8000")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
