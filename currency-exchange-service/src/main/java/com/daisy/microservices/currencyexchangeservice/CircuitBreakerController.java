657b6a0f1143ec480415b36c8174e72339567fef
Daisy <daisy.le0203@gmail.com>
2023-10-17 15:18:59 -0500

    playing with circuit breaker features of resilience4j

---
+++ currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.daisy.microservices.currencyexchangeservice;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+//    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
