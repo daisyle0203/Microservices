07524e30c6cf5b0c99a7825aa2a480713758e981
Daisy <daisy.le0203@gmail.com>
2023-10-17 15:31:26 -0500

    exploring rate limiting and builhead features of resilience4j

--- currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,11 +1,9 @@
 package com.daisy.microservices.currencyexchangeservice;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
@@ -14,11 +12,15 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+    // 10s -> 10000 calls to the sample api
+//    @RateLimiter(name="default")
+    @Bulkhead(name="default")
     public String sampleApi() {
         logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex) {
