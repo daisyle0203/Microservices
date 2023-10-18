65f0c34eb280a937ed94e78ee15d4e7150859d31
Daisy <daisy.le0203@gmail.com>
2023-10-17 22:16:00 -0500

    connecting currency exchange microservice with zipkin

--- currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/daisy/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.daisy.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private CurrencyExchangeRepository repository;
     // Get the value of the port
@@ -19,6 +22,8 @@ public class CurrencyExchangeController {
             @PathVariable String from,
             @PathVariable String to
     ) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         // CurrencyExchange currencyExchange = new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(50));
         // Instead of hardcoding the values, we will get the values from the database using JPA
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
