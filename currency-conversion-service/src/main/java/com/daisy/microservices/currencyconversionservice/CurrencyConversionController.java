9a425460a5dbf2221f0fa31bc97c2abeda5c9e03
Daisy <daisy.le0203@gmail.com>
2023-10-17 22:51:06 -0500

    connecting currency conversion microservice & API Gateway with Zipkin

--- currency-conversion-service/src/main/java/com/daisy/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/daisy/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,6 +1,9 @@
 package com.daisy.microservices.currencyconversionservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +13,24 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+// Best Practice: To integrate with RestTemplate with Micrometer,
+// we need to create RestTemplate using RestTemplateBuilder
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeProxy proxy;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertCurrency(
             @PathVariable String from,
@@ -25,7 +42,7 @@ public class CurrencyConversionController {
         uirVariables.put("to", to);
 
         // call the currency exchange service to get the conversion multiple
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                 CurrencyConversion.class, uirVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
